f37d57df316f2c1bbb717c8b2c4884d08a520ca3
Jonathan Muller <koren@pukogames.com>
2015-11-18 18:45:11 +0100

    Add basic file generator, pre-complete name and repo name

---
+++ generators/app/index.js
@@ -0,0 +1,64 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var fs = require('fs'),
+  ini = require('ini');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the super-excellent ' + chalk.red('generator-node-microservice') + ' generator!'
+    ));
+
+    var gitconfig;
+
+    try {
+      gitconfig = ini.parse(fs.readFileSync('./.git/config', 'utf-8'));
+    } catch(e){
+      gitconfig = {'remote "origin"': {url: ''}};
+    }
+
+    var prompts = [{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name:',
+      default : this.appname
+    },{
+      type    : 'input',
+      name    : 'git',
+      message : 'Github URL',
+      default : gitconfig['remote "origin"'].url
+    }, {
+      type    : 'confirm',
+      name    : 'amqp',
+      message : 'Does the microservice uses AMQP ?',
+      default : true
+    },{
+      type    : 'confirm',
+      name    : 'mongodb',
+      message : 'Does the microservice uses MongoDB ?',
+      default : true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copy(
+      this.templatePath(),
+      this.destinationPath()
+    );
+    this.template('package.json', 'package.json', this.props);
+  },
+
+  install: function () {
+    this.npmInstall();
+  }
+});
