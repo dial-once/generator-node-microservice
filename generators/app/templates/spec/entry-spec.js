0d7fdacb2f4e86fe6079ad84b86bbca37fa5e70b
Jonathan Muller <koren@pukogames.com>
2015-11-19 16:23:29 +0100

    Add basic test file

--- generators/app/templates/spec/entry-spec.js
+++ generators/app/templates/spec/entry-spec.js
@@ -1,6 +1,14 @@
-require('../../index');
+require('../index');
 var amqp = require('rabbitrpc')().producer;
 
-describe('', function(){
+describe('sample:test', function(){
+  beforeEach(function(done){
+    this.queue = 'sample:queue' + process.env.LOCAL_QUEUE;
+    done();
+  });
 
+  it('should be able to publish a message, and get a ack', function(done){
+    amqp.send(this.queue, {message: 'awesome!'})
+    .then(done);
+  });
 });
